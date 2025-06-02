89507abac06855a9080a3e2f565a3483fc06f2d3
Gaurav Aggarwal <gauravxor@gmail.com>
2025-06-03 01:56:02 +0530

    Initial commit

---
+++ src/main/java/com/clumsycoder/odinservice/OdinServiceApplication.java
@@ -0,0 +1,13 @@
+package com.clumsycoder.odinservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdinServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdinServiceApplication.class, args);
+    }
+
+}
