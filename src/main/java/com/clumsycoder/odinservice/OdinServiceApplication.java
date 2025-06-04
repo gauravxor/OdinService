3f6f442a1c849eb70175786ac65f0cb29f14b2ce
Gaurav Aggarwal <gauravxor@gmail.com>
2025-06-04 16:50:49 +0530

    Fix feign client annotations

--- src/main/java/com/clumsycoder/odinservice/OdinServiceApplication.java
+++ src/main/java/com/clumsycoder/odinservice/OdinServiceApplication.java
@@ -2,10 +2,11 @@ package com.clumsycoder.odinservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.clumsycoder.odinservice.clients")
 public class OdinServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OdinServiceApplication.class, args);
     }
--- src/main/java/com/clumsycoder/odinservice/clients/NucleusServiceClient.java
+++ src/main/java/com/clumsycoder/odinservice/clients/NucleusServiceClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service", url = "http://localhost:9000")
+@FeignClient(name = "nucleus-service", url = "http://localhost:9000")
 public interface NucleusServiceClient {
 
     @GetMapping("/api/player/{id}")
