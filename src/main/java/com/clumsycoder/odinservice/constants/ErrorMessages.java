3f6602e5660f476e1b80b2f7249cac070ca7e1c5
Gaurav Aggarwal <gauravxor@gmail.com>
2025-06-10 10:49:50 +0530

    Punctuate error messages

--- src/main/java/com/clumsycoder/odinservice/constants/ErrorMessages.java
+++ src/main/java/com/clumsycoder/odinservice/constants/ErrorMessages.java
@@ -7,28 +7,28 @@ public final class ErrorMessages {
     /**
      * Odin Service related exception
      */
-    public static final String ODIN_SERVICE_ERROR = "Something went wrong in OdinService";
+    public static final String ODIN_SERVICE_ERROR = "Something went wrong in OdinService.";
 
     /**
      * OTP related exception messages
      */
-    public static final String OTP_ERROR = "Something went wrong with OTP";
+    public static final String OTP_ERROR = "Something went wrong with OTP.";
 
     /**
      * JWT related exception messages
      */
-    public static final String JWT_ERROR = "Something went wrong with JWT";
+    public static final String JWT_ERROR = "Something went wrong with JWT.";
 
     /**
      * Nucleus Service related exception
      */
-    public static final String NUCLEUS_SERVICE_ERROR = "Error in Nucleus Service";
-    public static final String USERNAME_ALREADY_USED = "Username is not available";
-    public static final String EMAIL_ALREADY_USED = "Email is already used";
+    public static final String NUCLEUS_SERVICE_ERROR = "Error in Nucleus Service.";
+    public static final String USERNAME_ALREADY_USED = "Username is not available.";
+    public static final String EMAIL_ALREADY_USED = "Email is already used.";
     public static final String USER_NOT_FOUND = "User not found.";
-    public static final String USER_UPDATE_FAILED = "Failed to update the user";
-    public static final String USER_CREATE_FAILED = "Failed to create the user";
-    public static final String USER_DELETE_FAILED = "Failed to delete the user";
-    public static final String VALIDATION_ERROR = "Field validation error";
+    public static final String USER_UPDATE_FAILED = "Failed to update the user.";
+    public static final String USER_CREATE_FAILED = "Failed to create the user.";
+    public static final String USER_DELETE_FAILED = "Failed to delete the user.";
+    public static final String VALIDATION_ERROR = "Field validation error.";
 
 }
